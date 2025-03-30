eaef5d9c9753f8156811ae92b9fe446a9b67fc7a
taroshg <tarosh.g@gmail.com>
2025-03-30 11:34:38 -0400

    fastdds RPS

---
+++ src/DDSGameController.hpp
@@ -0,0 +1,230 @@
+#include "GameMessagePubSubTypes.hpp"
+
+#include <chrono>
+#include <thread>
+
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/topic/TypeSupport.hpp>
+
+#include <fastdds/dds/subscriber/DataReader.hpp>
+#include <fastdds/dds/subscriber/DataReaderListener.hpp>
+#include <fastdds/dds/subscriber/qos/DataReaderQos.hpp>
+#include <fastdds/dds/subscriber/SampleInfo.hpp>
+#include <fastdds/dds/subscriber/Subscriber.hpp>
+#include <fastdds/dds/publisher/DataWriter.hpp>
+#include <fastdds/dds/publisher/DataWriterListener.hpp>
+#include <fastdds/dds/publisher/Publisher.hpp>
+
+#include <iostream>
+#include <random>
+#include <sstream>
+
+using namespace eprosima::fastdds::dds;
+using namespace eprosima::fastdds::rtps;
+
+class PubListener : public DataWriterListener
+    {
+    private:
+        int matched_;
+    public:
+        PubListener() : matched_(0) {}
+        ~PubListener() override {}
+
+        void on_publication_matched(
+                DataWriter*,
+                const PublicationMatchedStatus& info) override
+        {
+            if (info.current_count_change == 1)
+            {
+                matched_ = info.total_count;
+                std::cout << "Publisher matched." << std::endl;
+            }
+            else if (info.current_count_change == -1)
+            {
+                matched_ = info.total_count;
+                std::cout << "Publisher unmatched." << std::endl;
+            }
+            else
+            {
+                std::cout << info.current_count_change
+                        << " is not a valid value for PublicationMatchedStatus current count change." << std::endl;
+            }
+        }
+
+        int matched() const {return matched_;}
+};
+
+class SubListener : public DataReaderListener
+{
+private:
+    int matched_;
+    unsigned long message_count_;
+    std::string last_uid_;
+    GameMessage game_msg_;
+public:
+    // we are sticking to 2 player games!
+    std::vector<std::string> participants_;
+
+    SubListener() : 
+    last_uid_(""),
+    matched_(0), 
+    message_count_(0), 
+    participants_({}){}
+    ~SubListener() override {}
+
+    void on_subscription_matched(
+            DataReader*,
+            const SubscriptionMatchedStatus& info) override
+    {
+        if (info.current_count_change == 1)
+        {   
+            matched_++;
+            std::cout << "Subscriber matched. " << std::endl;
+        }
+        else if (info.current_count_change == -1)
+        {
+            matched_--;
+            std::cout << "Subscriber unmatched." << std::endl;
+        }
+        else
+        {
+            std::cout << info.current_count_change
+                    << " is not a valid value for SubscriptionMatchedStatus current count change" << std::endl;
+        }
+    }
+
+    void on_data_available(DataReader* reader) override
+    {
+        SampleInfo info;
+        if (reader->take_next_sample(&game_msg_, &info) == eprosima::fastdds::dds::RETCODE_OK && info.valid_data)
+        {
+            // adds the participant uid to a list
+            last_uid_ = game_msg_.uid();
+            participants_.insert(participants_.begin() + message_count_, game_msg_.uid());
+            message_count_++;
+        }
+    }
+
+    int matched() const {return matched_;}
+    unsigned long message_count() const {return message_count_;}
+    std::vector<std::string>& participants() {return participants_;}
+    std::string last_uid() const {return last_uid_;}
+    GameMessage* game_msg() {return &game_msg_;}
+};
+
+class DDSGameController{
+private:
+    DomainParticipant* participant_;
+    Publisher* publisher_;
+    Subscriber* subscriber_;
+    Topic* topic_;
+    DataWriter* writer_;
+    DataReader* reader_;
+    TypeSupport type_;
+
+    PubListener pub_listener_;
+    SubListener sub_listener_;
+
+    unsigned long last_message_count_;
+    std::string uid_;
+
+    std::string generateUUID() {
+        std::random_device rd;
+        std::mt19937_64 gen(rd());
+        std::uniform_int_distribution<uint64_t> dis;
+    
+        uint64_t uuid = dis(gen);
+    
+        std::stringstream ss;
+        ss << std::hex << uuid;
+        return ss.str();
+    }
+
+public:
+    DDSGameController() : participant_(nullptr)
+    , publisher_(nullptr)
+    , topic_(nullptr)
+    , writer_(nullptr)
+    , reader_(nullptr)
+    , last_message_count_(0)
+    , type_(new GameMessagePubSubType()) {}
+
+    virtual ~DDSGameController()
+    {
+        if (writer_ != nullptr)
+        {
+            publisher_->delete_datawriter(writer_);
+        }
+        if (publisher_ != nullptr)
+        {
+            participant_->delete_publisher(publisher_);
+        }
+        if (topic_ != nullptr)
+        {
+            participant_->delete_topic(topic_);
+        }
+        DomainParticipantFactory::get_instance()->delete_participant(participant_);
+    }
+
+    bool init(){
+        // creates a new participant
+        DomainParticipantQos participantQos;
+        participantQos.name("Participant_controller");
+        participant_ = DomainParticipantFactory::get_instance()->create_participant(0, participantQos);
+
+        if (participant_ == nullptr) return false;
+        type_.register_type(participant_);
+
+        // create a topic
+        topic_ = participant_->create_topic("GameTopic", "GameMessage", TOPIC_QOS_DEFAULT);
+        if (topic_ == nullptr) return false;
+
+        // creates publisher and writer
+        publisher_ = participant_->create_publisher(PUBLISHER_QOS_DEFAULT, nullptr);
+        if (publisher_ == nullptr) return false;
+        writer_ = publisher_->create_datawriter(topic_, DATAWRITER_QOS_DEFAULT, &pub_listener_);
+        if (writer_ == nullptr) return false;
+
+        // creates subscriber and reader
+        subscriber_ = participant_->create_subscriber(SUBSCRIBER_QOS_DEFAULT, nullptr);
+        if (subscriber_ == nullptr) return false;
+        reader_ = subscriber_->create_datareader(topic_, DATAREADER_QOS_DEFAULT, &sub_listener_);
+        if (reader_ == nullptr) return false;
+
+        uid_ = generateUUID();
+
+        return true;
+    }
+
+    std::string uid() const {return uid_;}
+
+    bool publish(GameMessage* game){
+        if (pub_listener_.matched() > 0){
+            writer_->write(game);
+            return true;
+        }
+        return false;
+    }
+
+    int n_subscribers() const{
+        return sub_listener_.matched();
+    } 
+
+    int n_publishers() const{
+        return pub_listener_.matched();
+    }
+
+    std::string last_uid() const {return sub_listener_.last_uid();}
+
+    unsigned long message_count() const {
+        return sub_listener_.message_count();
+    }
+
+    std::vector<std::string>& participants() {return sub_listener_.participants();}
+
+    GameMessage* read() {
+        // read when there is a new message to be read
+        return sub_listener_.game_msg();
+    }
+};
--- src/GameUser.cpp
+++ src/GameUser.cpp
@@ -1,87 +1,211 @@
-#include "GameSubscriber.hpp"
-#include "GamePublisher.hpp"
+#include "DDSGameController.hpp"
 
 #include <iostream>
 #include <thread>
-#include <random>
-#include <sstream>
+#include <chrono>
 
-bool send(GamePublisher* pub, Game* game, std::string uid){
-    game->uid(uid);
+class GameWrapper {
+protected:
+    int my_wins_;
+    int opp_wins_;
+    const bool end_on_full_turn_;
+    int my_moves_;
+    int opp_moves_;
+public:
+    GameWrapper(bool end_on_full_turn) : 
+    my_wins_(0),
+    opp_wins_(0),
+    end_on_full_turn_(end_on_full_turn) {}
 
-    std::string msg;
-    
-    std::getline(std::cin, msg);
-    std::cout << "\033[A\33[2K\r";
-    
-    game->message(msg);
-    if(pub->publish(game)){
-        return true;
-    }
-    std::cout << "missed send!";
-    return false;
-}
+    virtual void get_user_move(GameMessage* game_msg) = 0;
+    virtual int get_winner(GameMessage* game_msg, GameMessage* opp_game_msg) = 0;
 
-bool get(GameSubscriber* sub, Game* game, std::string uid){
-    if(sub->get(game) && game->uid() != uid){
-        std::cout << "msg from " << game->uid() << ": " << game->message() << "\n";
-        return true;
-    }
-    return false;
-}
+    int get_my_wins() const {return my_wins_;}
+    int get_opp_wins() const {return opp_wins_;}
 
-std::atomic<bool> running(true);
+    bool end_on_full_turn() const {return end_on_full_turn_;}
 
-void send_worker(GamePublisher* pub, Game* game, std::string uid){
-    while (running){
-        if (game->message() == "quit!"){
-            running = false;
-            break;
+    void I_moved() {my_moves_++;}
+    void opp_moved() {opp_moves_++;}
+
+    int my_moves() const {return my_moves_;}
+    int opp_moves() const {return opp_moves_;} 
+
+    bool is_full_turn() const {return my_moves_ == opp_moves_;}
+};
+
+class RPS : public GameWrapper {
+public:
+    RPS(): GameWrapper(true) {};
+    void get_user_move(GameMessage* game_msg) override {
+        char move;
+        do {
+            std::cout << "'r' for rock\n";
+            std::cout << "'p' for paper\n";
+            std::cout << "'s' for scissors\n";
+            std::cout << "Enter your choice: ";
+            std::cin >> move;
+        } while (move != 'r' && move != 'p' && move != 's');
+        
+        switch(move){
+            case 'r': game_msg->rps(0); break;
+            case 'p': game_msg->rps(1); break;
+            case 's': game_msg->rps(2); break;
         }
-        send(pub, game, uid);
     }
-}
 
-void get_worker(GameSubscriber* sub, Game* game, std::string uid){
-    while (running){
-        get(sub, game, uid);
+    int get_winner(GameMessage* game_msg, GameMessage* opp_game_msg) override {
+        long move = game_msg->rps();
+        long opp_move = opp_game_msg->rps();
+
+        if (move == opp_move) return 0;
+
+        // rock vs paper
+        switch(move){
+            case 0: // rock
+                if (opp_move == 1) return -1; // rock vs paper 
+                else return 1; // rock vs scissors
+            case 1: // paper
+                if (opp_move == 0) return 1; // paper vs rock 
+                else return -1; // paper vs scissors
+            case 2: // 
+                if (opp_move == 0) return -1; // scissors vs rock 
+                else return 1; // scissors vs paper
+        }
+        return 0;
     }
-}
+};
 
-std::string generateUUID() {
-    std::random_device rd;
-    std::mt19937_64 gen(rd());
-    std::uniform_int_distribution<uint64_t> dis;
-
-    uint64_t uuid = dis(gen);
-
-    std::stringstream ss;
-    ss << std::hex << uuid;
-    return ss.str();
-}
-
-int main(){
-
-    GameSubscriber sub;
-    GamePublisher pub;
-
-    std::string uid = generateUUID();
-
-    std::cout << "created a user with " << uid;
-
-    Game myGame;
-    Game oppGame;
-
-    bool sent = false;
-    bool received = false;
-
-    bool run = true;
-    if (pub.init() && sub.init()){
+class GameUser{
+private:
+    unsigned long last_message_count_;
+    DDSGameController my_controller_;
+    GameWrapper* game_api_;
     
-        std::thread pub_td(send_worker, &pub, &myGame, uid);
-        std::thread sub_td(get_worker, &sub, &oppGame, uid);
+public:
+    std::string uid_; // my ID
+    std::string oid_; // opponent ID
 
-        pub_td.join();
-        sub_td.join();
+    bool turn_;
+    GameUser(GameWrapper* game_api): last_message_count_(0), 
+                                 turn_(false) {
+                                    game_api_ = game_api;
+                                 }
+
+    bool init(){
+        if (my_controller_.init()){
+            while(my_controller_.n_publishers() < 2){
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            }
+
+            uid_ = my_controller_.uid();
+            std::cout << "ME: " << uid_ << std::endl;
+
+            GameMessage game_msg;
+            game_msg.uid(uid_);
+            game_msg.message("connected!");
+            if (!my_controller_.publish(&game_msg)) return false;
+    
+            // wait until there are more than 1 broadcast, until another player joins
+            while(my_controller_.message_count() < 2){
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            }
+            
+
+            // participants are going to be in the order of first joined
+            int i = 0;
+            for (std::string id : my_controller_.participants()){
+                if (id != uid_){
+                    oid_ = id;
+                    break;
+                }
+                i++;
+            }
+            std::cout << "OPP: " << oid_ << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+            // if you are first participant, you start!
+            turn_ = my_controller_.participants()[0] == uid_;
+
+            last_message_count_ = my_controller_.message_count();
+
+            if (turn_){
+                std::cout << "you go first!" << std::endl;
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+    void play(int nrounds){
+        int round = 0;
+        GameMessage* opp_game_msg = new GameMessage();
+        GameMessage* my_game_msg = new GameMessage();
+
+        while(round < nrounds){
+            // read if there is a new message and is from opp
+            if (my_controller_.message_count() > last_message_count_){
+                opp_game_msg = my_controller_.read();
+                last_message_count_ = my_controller_.message_count(); 
+                
+                if (opp_game_msg->uid() != oid_) continue;
+
+                std::cout << "opp moved!" << std::endl;
+                turn_ = true;
+                game_api_->opp_moved();
+
+                // rps specific!!
+                if (game_api_->is_full_turn() && game_api_->end_on_full_turn()){
+                    std::cout << "-------- round " << round << " -------" << std::endl;
+                    // print moves
+                    std::cout << "my (" << my_game_msg->uid() << ") move: " << my_game_msg->rps() << std::endl;
+                    std::cout << "opp (" << opp_game_msg->uid() << ") move: " << opp_game_msg->rps() << std::endl;
+
+                    round++;
+                    int score = game_api_->get_winner(my_game_msg, opp_game_msg);
+                    if(score > 0) std::cout << "--- you won! ---" << std::endl;
+                    else if(score < 0) std::cout << "--- you lose :( ---" << std::endl;
+                    else std::cout << "--- tie ---" << std::endl;
+                }
+            }
+            
+            if (turn_){
+                my_game_msg->uid(uid_);
+
+                game_api_->get_user_move(my_game_msg);
+                game_api_->I_moved();
+
+                // after my move, if there is opp move
+                if (my_controller_.last_uid() == oid_){
+
+                    // rps specific!! (determine winner)
+                    if (game_api_->is_full_turn() && game_api_->end_on_full_turn()){
+                        std::cout << "-------- round " << round << " -------" << std::endl;
+                        // print moves
+                        std::cout << "my (" << my_game_msg->uid() << ") move: " << my_game_msg->rps() << std::endl;
+                        std::cout << "opp (" << opp_game_msg->uid() << ") move: " << opp_game_msg->rps() << std::endl;
+
+                        round++;
+                        int score = game_api_->get_winner(my_game_msg, opp_game_msg);
+                        if(score > 0) std::cout << "--- you won! ---" << std::endl;
+                        else if(score < 0) std::cout << "--- you lose :( ---" << std::endl;
+                        else std::cout << "--- tie ---" << std::endl;
+                    }
+                }
+
+                // publish my move
+                if(my_controller_.publish(my_game_msg)){
+                    turn_ = false;
+                }
+            }
+        }
+    }
+};
+int main(){
+    GameWrapper* rps = new RPS();
+    GameUser* user = new GameUser(rps);
+    if(user->init()){
+        user->play(3);
     }
 }
